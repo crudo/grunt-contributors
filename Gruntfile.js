8abf7d107de3576e42bc1bc17dfe0a447ff76b14
Martin Cermak <crudo@crudo.cz>
2014-03-09 17:07:42 +0100

    CODE: 0.0.1

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+/*
+* grunt-contributors
+* https://github.com/crudo/grunt-contributors
+*
+* Copyright (c) crudo <crudo@crudo.cz>
+* Licensed under the MIT license.
+*/
+
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: [
+            'Gruntfile.js',
+            'tasks/*.js',
+            '<%= nodeunit.tests %>',
+            ],
+            options: {
+                jshintrc: '.jshintrc',
+            },
+        },
+
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            tests: ['tmp'],
+        },
+
+        // Configuration to be run (and then tested).
+        contributors: {
+            options: {
+                readpath: 'test/fixtures/package.json',
+                writepath: 'tmp/package.json',
+                add: [{
+                    name: 'Aron Bold',
+                    email: 'someone@example.com'
+                }]
+            }
+        },
+
+        // Unit tests.
+        nodeunit: {
+            tests: ['test/*_test.js'],
+        }
+    });
+
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    grunt.registerTask('test', ['clean', 'contributors', 'nodeunit']);
+
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'test']);
+};
